47031bcd0d5433d9ef5b7877a3d01a9a2737183e
Antonio Gasco <antoniofgasco@gmail.com>
2022-08-01 10:33:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Header</div>;
 }
 
 export default App;
+
+/*
+ * - Fetch data and save it in global state (redux)
+ * - Render list
+ * - Be able to add, modify, delete any element of the list
+ * - Mobile-first approach
+ */
